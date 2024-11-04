72a28fcc1b3fe7b84f2b9bf9ae371c0aea8abf20
Xuan584 <xuangarciaro@gmail.com>
2024-11-04 13:22:21 +0100

    Biblioteca.php terminado

--- biblioteca.php
+++ biblioteca.php
@@ -18,6 +18,45 @@ $libros = [
         "precio" => 29.99,  
         "categoria" => "Algoritmos"  
     ]  
-    
 ];
 
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Biblioteca de Libros</title>
+</head>
+<body>
+
+<h2>Información de todos los libros</h2>
+<table border="2px" style="border-collapse: collapse;">
+    <tr>
+        <th>Título</th>
+        <th>Autor</th>
+        <th>Precio</th>
+        <th>Categoría</th>
+    </tr>
+    <?php foreach ($libros as $libro) : ?>
+        <tr>
+            <td><?php echo $libro['titulo']; ?></td>
+            <td><?php echo $libro['autor']; ?></td>
+            <td><?php echo $libro['precio']; ?></td>
+            <td><?php echo $libro['categoria']; ?></td>
+        </tr>
+    <?php endforeach; ?>
+</table>
+
+<h2>Libros de la categoría Desarrollo Web</h2>
+<ol>
+    <?php foreach ($libros as $libro) : ?>
+        <?php if ($libro['categoria'] === 'Desarrollo web') : ?>
+            <li><?php echo $libro['titulo']; ?></li>
+        <?php endif; ?>
+    <?php endforeach; ?>
+</ol>
+
+</body>
+</html>
